c5d757a0a7e7784d0506ff6bfa6677c33ffbd404
Rashchikov_Kirill <dsydolg123@yandex.ru>
2023-03-21 16:43:20 +0300

    Initial commit

---
+++ src/main/java/com/example/Task14/Task14Application.java
@@ -0,0 +1,12 @@
+package com.example.Task14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task14Application.class, args);
+	}
+}
